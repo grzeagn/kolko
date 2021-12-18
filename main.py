0bf8ac0689babf23ddd7693a834c801328457cd6
grzeagn <grzeagn@poczta.onet.pl>
2021-12-18 11:06:59 +0100

    poprawki

--- main.py
+++ main.py
@@ -15,7 +15,26 @@ def drukujPlansze(pole):
     print(f"{pole['4']}|{pole['5']}|{pole['6']}")
     print('-+-+-')
     print(f"{pole['1']}|{pole['2']}|{pole['3']}")
-drukujPlansze(PlanszaDoGry)
+#drukujPlansze(PlanszaDoGry)
+
+def gra():
+
+    gracz='X'
+    licznik=0
+
+    for i in range(10):
+        drukujPlansze(PlanszaDoGry)
+        move=input(f'To jest ruch, {gracz}. Wybierz gdzie chcesz ustawić znak!')
+
+        if PlanszaDoGry[move]==' ':
+            PlanszaDoGry[move]=gracz
+            licznik +=1
+        else:
+            print('Miejsce jest juz zajete!!!\nWstaw swój znak gdzieś indziej!')
+            continue
+
+        if licznik >=5:
+            
 
 
     
