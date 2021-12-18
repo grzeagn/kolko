cbff9e96b4100b7d727095ed235f80402c01683a
grzeagn <grzeagn@poczta.onet.pl>
2021-12-18 10:30:14 +0100

    skonczona plansza do gry

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 #%%
 # tu bedzie gra kolko i krzyzyk
-print()
-# test programu
+
+PlanszaDoGry = {'7':' ','8':' ' ,'9':' ',
+                '4':' ','5':' ','6':' ',
+                '1':' ','2':' ','3':' '}
+klawiszeGry=[]
+
+for key in PlanszaDoGry:
+    klawiszeGry.append(key)
+
+def drukujPlansze(pole):
+    print(f"{pole['7']}|{pole['8']}|{pole['9']}")
+    print('-+-+-')
+    print(f"{pole['4']}|{pole['5']}|{pole['6']}")
+    print('-+-+-')
+    print(f"{pole['1']}|{pole['2']}|{pole['3']}")
+drukujPlansze(PlanszaDoGry)
+
+
+    
+
+# %%
