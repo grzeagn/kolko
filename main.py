baf2477c38e9d752280c83a224e4379a522fc87e
grzeagn <grzeagn@poczta.onet.pl>
2021-12-18 12:07:11 +0100

    gotowy program

--- main.py
+++ main.py
@@ -34,9 +34,72 @@ def gra():
             continue
 
         if licznik >=5:
+            if  PlanszaDoGry['7']==PlanszaDoGry['8']==PlanszaDoGry['9']!='':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ:{gracz}')
+                break
+            elif  PlanszaDoGry['4']==PlanszaDoGry['5']==PlanszaDoGry['6']!='':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ:{gracz}')
+                break
+            elif  PlanszaDoGry['1']==PlanszaDoGry['2']==PlanszaDoGry['3']!='':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ:{gracz}')
+                break
+            #Tutaj konczą się wygrane poziome
+            elif PlanszaDoGry['7']==PlanszaDoGry['4']==PlanszaDoGry['1']!= ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ: {gracz}')
+                break
+
+            elif PlanszaDoGry['8']==PlanszaDoGry['5']==PlanszaDoGry['2']!= ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ: {gracz}')
+                break
+
+            elif PlanszaDoGry['9']==PlanszaDoGry['6']==PlanszaDoGry['3']!= ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ: {gracz}')
+                break
             
+#wygrane przekątne
+            elif PlanszaDoGry['7']==PlanszaDoGry['5']==PlanszaDoGry['3']!= ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ: {gracz}')
+                break
+            
+            elif PlanszaDoGry['9']==PlanszaDoGry['5']==PlanszaDoGry['1']!= ' ':
+                drukujPlansze(PlanszaDoGry)
+                print('\nKONIEC GRY!\n')
+                print(f'WYGRAŁ GRACZ: {gracz}')
+                break
 
 
-    
+            if licznik==9:
+                print('\nKONIEC GRY!!!\n')
+                print('\nJEST REMIS!!!\n')
 
-# %%
+        if gracz =='X':
+            gracz='0'
+        else:
+            gracz='X'
+    restart=input('Czy chcesz zagrac ponownie/(t/n')
+    if restart=='t' or restart=='T':
+        for key in klawiszeGry:
+            PlanszaDoGry[key]=''
+        gra()
+
+if __name__ == '__main__':
+    gra()
+                
+
+
+#%%
+print()
